ddbcab15647a972159e83556c06ac1832f1e9cdb
smschlts <semschol@gmail.com>
2020-07-27 16:19:34 +0200

    Eerste commit

---
+++ src/main/java/nl/workingtalent/bieb/BiebApplication.java
@@ -0,0 +1,13 @@
+package nl.workingtalent.bieb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiebApplication.class, args);
+	}
+
+}
