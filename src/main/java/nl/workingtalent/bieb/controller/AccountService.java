aa68e9d7d04ae111f21475c72a37efee1aa8e073
FT <21099480+Bla@users.noreply.github.com>
2020-07-29 12:28:15 +0200

    account endpoints

--- src/main/java/nl/workingtalent/bieb/controller/AccountService.java
+++ src/main/java/nl/workingtalent/bieb/controller/AccountService.java
@@ -1,10 +1,13 @@
 package nl.workingtalent.bieb.controller;
 
 
+import nl.workingtalent.bieb.domein.Account;
+import nl.workingtalent.bieb.domein.Boek;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 @Transactional
@@ -14,5 +17,40 @@ public class AccountService {
 
     //Hier komen alle methodes die met de database te maken hebben.
 
+    public Account opslaan(Account nieuwAccount) {
+        System.out.println("Account opslaan");
+        return accountRepository.save(nieuwAccount);
+    }
+
+    public List<Account> ophalenAlleAccounts() {
+        System.out.println("Alle accounts ophalen");
+        return (List<Account>) accountRepository.findAll();
+    }
+
+    public Account ophalenAccount(long id) {
+        System.out.println("Haal account " + id + " op");
+        return accountRepository.findById(id).orElse(null);
+    }
+
+    public void verwijderAccount(long id) {
+        System.out.println("Verwijder account " + id);
+        accountRepository.deleteById(id);
+    }
+
+    //TODO ???
+    public Account updateAccount(long id, Account nieuwAccount) {
+        System.out.println("Update account " + id);
+        Account bestaandAccount = accountRepository.findById(id).orElse(null);
+
+        if (bestaandAccount != null) {
+            bestaandAccount.setNaam(nieuwAccount.getNaam());
+            bestaandAccount.setEmail(nieuwAccount.getEmail());
+            bestaandAccount.setWachtwoord(nieuwAccount.getWachtwoord());
+
+            return accountRepository.save(bestaandAccount);
+        } else {
+            return accountRepository.save(nieuwAccount);
+        }
+    }
 
 }
--- src/main/java/nl/workingtalent/bieb/rest/AccountEndpoint.java
+++ src/main/java/nl/workingtalent/bieb/rest/AccountEndpoint.java
@@ -2,20 +2,40 @@ package nl.workingtalent.bieb.rest;
 
 
 import nl.workingtalent.bieb.controller.AccountService;
+import nl.workingtalent.bieb.domein.Account;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class AccountEndpoint {
     @Autowired
     AccountService accountService;
 
-    @GetMapping("/account")
-    public String accounts() {
-        System.out.println("hij doet het");
-        return "test";
+    @GetMapping("/accounts")
+    public List<Account> accountsAlles() {
+        return accountService.ophalenAlleAccounts();
     }
 
+    @GetMapping("/accounts/{id}")
+    public Account AccountEen(@PathVariable Long id) {
+        return accountService.ophalenAccount(id);
+    }
+
+    @PostMapping("/accounts")
+    public Account nieuwAccount(@RequestBody Account nieuwAccount) {
+        return accountService.opslaan(nieuwAccount);
+    }
+
+    @DeleteMapping("/accounts/{id}")
+    public void accountWeg(@PathVariable Long id) {
+        accountService.verwijderAccount(id);
+    }
+
+    @PutMapping("/accounts/{id}")
+    public Account updateAccount(@PathVariable Long id, @RequestBody Account nieuwAccount) {
+        return accountService.updateAccount(id, nieuwAccount);
+    }
 
 }
